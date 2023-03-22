a18626e81efa31309623a7a34ec4867051e70557
VargasSantiago <vargas.santiago2002@gmail.com>
2023-03-22 11:08:03 -0300

    Primer commit

---
+++ src/main/java/com/example/apirest/APIRESTApplication.java
@@ -0,0 +1,15 @@
+package com.example.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIRESTApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(APIRESTApplication.class, args);
+
+	}
+
+}
